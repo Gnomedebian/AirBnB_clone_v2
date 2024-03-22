b103f9c48773643802a7885d0e3a8c089e5f9f7e
Gnomedebian <yassineamgarou@gmail.com>
2024-03-22 11:30:58 +0000

    task::web_flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Starts a Flask web application.
+"""
 
 from flask import Flask
 
@@ -12,4 +15,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
