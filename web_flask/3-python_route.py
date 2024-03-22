d0aea3747138aed068ad42da07c40bebda44f0a6
Gnomedebian <yassineamgarou@gmail.com>
2024-03-22 12:04:35 +0000

    task::py_route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -27,6 +27,7 @@ def text(text):
     return f'C {text}'
 
 
+@app.route("/python", strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def text_py(text = 'is cool'):
     '''function returns variable'''
