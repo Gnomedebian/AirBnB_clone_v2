d46c22dc8ec8dda5b0ae19e57409c40734a64d0c
Gnomedebian <yassineamgarou@gmail.com>
2024-03-22 11:57:16 +0000

    task::py_route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def greetHbnb():
+    '''function returns hello HBNB!'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''function returns HBNB'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def text(text):
+    '''function returns variable'''
+    text = text.replace('_', ' ')
+    return f'C {text}'
+
+
+@app.route('/python/<text>')
+def textPy(textPy):
+    '''function returns variable'''
+    new_text = textPy.replace('_', ' ')
+    return f'C {new_text}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
