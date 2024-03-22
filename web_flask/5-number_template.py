31675bc998595962518544b4ee98976cf3ef04b5
Gnomedebian <yassineamgarou@gmail.com>
2024-03-22 12:55:39 +0000

    task::num_temp

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -43,10 +43,12 @@ def number(n):
 
 app = Flask(__name__)
 
+
 @app.route('/number_template/<int:n>')
 def number_template(n):
     '''Displays an HTML page only if <n> is an integer'''
     return render_template('5-number.html', n=n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
